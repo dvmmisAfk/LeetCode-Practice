7962e663ead7ee646b29076d3a7e3acbd8d45a7e
Divyam <divyam11nov@gmail.com>
2026-06-23 00:22:13 +0530

    Time: 1 ms (99.82%), Space: 46.3 MB (99.91%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroIndex = 0;
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
+            }
+        }
+        // Fill the remaining positions with zeros
+        for (int i = nonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
